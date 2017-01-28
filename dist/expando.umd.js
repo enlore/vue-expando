0616aae1ef493d102ae2cd95284b51807bc518ed
enlore <n.e.lorenson@gmail.com>
2017-01-27 22:29:20 -0600

    Dist and ignore

---
+++ dist/expando.umd.js
@@ -0,0 +1,56 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.Expando = factory());
+}(this, (function () { 'use strict';
+
+var expando = { template: "<div class=\"vue-expando\" v-content-expand><div class=\"vue-expando-transitionWrap\" v-content-get-height><div class=\"vue-expando-header\"><h4 class=\"vue-expando-title\">{{ title }}</h4><p class=\"vue-expando-subtitle\">{{ subtitle }}</p><span class=\"vue-expando-control\" @click=\"toggleOpen\"><span v-if=\"panelIsOpen\">Collapse </span><span v-else>Expand</span></span></div><div class=\"vue-expando-content\"><slot></slot></div></div></div>",
+    data: function data () {
+        return {
+            panelIsOpen: false,
+            contentHeight: 0,
+            baseHeight: 104
+        }
+    },
+
+    computed: {
+    },
+
+    methods: {
+        toggleOpen: function toggleOpen () {
+            if (this.panelIsOpen) { // close it
+                this.expandEl.style.height = this.baseHeight + 'px';
+            } else { // open it
+                this.expandEl.style.height = this.contentHeight + 'px';
+            }
+
+            // flip the val
+            this.panelIsOpen = !this.panelIsOpen;
+        }
+    },
+
+    directives: {
+        'content-expand': {
+            inserted: function inserted (el, binding, vnode) {
+                // grab reference to element we want to expand/contract
+                vnode.context.expandEl = el;
+            }
+        },
+
+        'content-get-height': {
+            inserted: function inserted (el, binding, vnode) {
+                // calculated height of content in target element
+                vnode.context.contentHeight = el.clientHeight;
+            }
+        }
+    },
+
+    props: [
+        'title',
+        'subtitle'
+    ]
+};
+
+return expando;
+
+})));
