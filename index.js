7fea6dd3654466b22d87bbb73821a6f540fe7bb1
enlore <n.e.lorenson@gmail.com>
2017-01-27 22:22:18 -0600

    Rollup to bundle it for umd, es

--- index.js
+++
@@ -1,6 +0,0 @@
-/* jshint node: true, asi: true, laxcomma: true, esversion: 6 */
-'use strict'
-
-import Expando from './expando.vue'
-
-export default Expando
---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+/* jshint node: true, asi: true, laxcomma: true, esversion: 6 */
+'use strict'
+
+import vue from 'rollup-plugin-vue'
+import buble from 'rollup-plugin-buble'
+
+export default {
+    entry: 'src/expando.vue',
+    targets: [
+        { dest: 'dist/expando.umd.js', format: 'umd', moduleName: 'Expando' },
+        { dest: 'dist/expando.js', format: 'es' },
+    ],
+    plugins: [
+        vue({
+            css: 'dist/expando.css'
+        }),
+        buble()
+    ]
+}
