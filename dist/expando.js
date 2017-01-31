7be2c233ac627e11cf87512dd7fb007289939321
enlore <n.e.lorenson@gmail.com>
2017-01-31 14:25:32 -0600

    Have to make a build in order to push a build

--- dist/expando.js
+++ dist/expando.js
@@ -1,13 +1,16 @@
-var expando = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{directives:[{name:"content-expand",rawName:"v-content-expand"}],staticClass:"vue-expando"},[_c('div',{directives:[{name:"content-get-height",rawName:"v-content-get-height"}],staticClass:"vue-expando-transitionWrap"},[_c('div',{staticClass:"vue-expando-header"},[_c('h4',{staticClass:"vue-expando-title"},[_vm._v(_vm._s(_vm.title))]),_c('p',{staticClass:"vue-expando-subtitle"},[_vm._v(_vm._s(_vm.subtitle))]),_c('span',{staticClass:"vue-expando-control",on:{"click":_vm.toggleOpen}},[(_vm.panelIsOpen)?_c('span',[_vm._v("Collapse ")]):_c('span',[_vm._v("Expand")])])]),_c('div',{staticClass:"vue-expando-content"},[_vm._t("default")],2)])])},staticRenderFns: [],
+var expando = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{directives:[{name:"content-expand",rawName:"v-content-expand"}],staticClass:"vue-expando"},[_c('div',{directives:[{name:"content-get-height",rawName:"v-content-get-height"}],staticClass:"vue-expando-transitionWrap vue-expando-content"},[_c('div',{staticClass:"vue-expando-header"},[_c('span',{staticClass:"vue-expando-control",on:{"click":_vm.toggleOpen}},[(_vm.panelIsOpen)?_c('span',[_vm._v("Collapse ")]):_c('span',[_vm._v("Expand")])]),_vm._t("header")],2),_c('div',{staticClass:"vue-expando-body"},[_vm._t("body")],2)])])},staticRenderFns: [],
     data: function data () {
         return {
             panelIsOpen: false,
-            contentHeight: 0,
-            baseHeight: 104
+            bodyHeight: 0,
+            _baseHeight: 96
         }
     },
 
     computed: {
+        baseHeight: function baseHeight () {
+            return this.initialHeight || this._baseHeight
+        }
     },
 
     methods: {
@@ -15,7 +18,7 @@ var expando = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=
             if (this.panelIsOpen) { // close it
                 this.expandEl.style.height = this.baseHeight + 'px';
             } else { // open it
-                this.expandEl.style.height = this.contentHeight + 'px';
+                this.expandEl.style.height = this.bodyHeight + 'px';
             }
 
             // flip the val
@@ -33,15 +36,14 @@ var expando = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=
 
         'content-get-height': {
             inserted: function inserted (el, binding, vnode) {
-                // calculated height of content in target element
-                vnode.context.contentHeight = el.clientHeight;
+                // calculated height of body in target element
+                vnode.context.bodyHeight = el.clientHeight;
             }
         }
     },
 
     props: [
-        'title',
-        'subtitle'
+        'initialHeight'
     ]
 };
 
